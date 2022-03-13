d3000e685913b5a2036c5b8f2833f49b5dbe62c3
dennis <wuaoliu52@gmail.com>
2022-03-12 20:14:01 -0500

    add data and intrinsics

--- SfM.py
+++ SfM.py
@@ -1,1 +1,33 @@
-print("hello world from SfM!")
+import cv2
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io
+from utils.cameraParams import generateIntrinsics
+img1 = plt.imread('./data/image1.jpg')
+img2 = plt.imread('./data/image2.jpg')
+img3 = plt.imread('./data/image3.jpg')
+img4 = plt.imread('./data/image4.jpg')
+img5 = plt.imread('./data/image5.jpg')
+
+def plot_imgs():
+  fig, ax = plt.subplots(3, 2)
+  for a in ax.reshape((-1)):
+    a.set_axis_off()
+  ax[0][0].imshow(img1)
+  ax[0][1].imshow(img2)
+  ax[1][0].imshow(img3)
+  ax[1][1].imshow(img4)
+  ax[2][0].imshow(img5)
+  plt.show()
+# plot_imgs()
+
+
+if __name__ == '__main__':
+  print("hello world from SfM!")
+  intrinsics = generateIntrinsics()
+
+
+
+
+
---
+++ utils/cameraParams.py
@@ -0,0 +1,22 @@
+def generateIntrinsics():
+    intrinsics = {}
+    intrinsics['FocalLength'] = [1037.575214664696, 1043.315752317925]
+    intrinsics['PrincipalPoint'] = [642.2315830312182, 387.8357750962377]
+    intrinsics['ImageSize'] = [720, 1280]
+    intrinsics['RadialDistortion'] = [0.146911684283474, -0.214389634520344]
+    intrinsics['TangentialDistortion'] = [0., 0.]
+    intrinsics['Skew'] = 0.
+    intrinsics['IntrinsicMatrix'] = [[1037.575214664696,    0.               ,      0.],
+                                     [0.               ,    1043.315752317925,      0.],
+                                     [642.2315830312182,	387.8357750962377,  	1.]]
+    return intrinsics
+# intrinsics = {}
+# intrinsics['FocalLength'] = [1037.575214664696, 1043.315752317925]
+# intrinsics['PrincipalPoint'] = [642.2315830312182, 387.8357750962377]
+# intrinsics['ImageSize'] = [720, 1280]
+# intrinsics['RadialDistortion'] = [0.146911684283474, -0.214389634520344]
+# intrinsics['TangentialDistortion'] = [0., 0.]
+# intrinsics['Skew'] = 0.
+# intrinsics['IntrinsicMatrix'] = [[1037.575214664696,    0.               ,      0.],
+#                                  [0.               ,    1043.315752317925,      0.],
+#                                  [642.2315830312182,	387.8357750962377,  	1.]]
