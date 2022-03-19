f077f6a47a26991d4841fcde4e01d4ce184113c4
dennis <wuaoliu52@gmail.com>
2022-03-18 23:35:05 -0400

    add dataloader and test

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -3,48 +3,54 @@ import h5py
 import glob
 from scipy.spatial.transform import Rotation as R
 def load_7scenes_data(scene):
-    driverpath = '/home/dennis/indoor_localization/data/7Scenes/';
+
     print('=>>>>>>>>>>>>>>>>>>>>>>Loading 7Scenes dataset', scene)
-    # load('7scenes/cameraParams_kinect_7scenes.mat');
+    print('=>>>>>>>>>>>>>>>>>>>>>>Loading testing data')
     dset_test_data_path = '/home/dennis/indoor_localization/data/7Scenes/' + scene + '/sequences/01/'
-    # test_data_path = '/home/dennis/indoor_localization/data/7Scenes/' + subdataset + '/sequences/01/'
     dset_test_names = glob.glob(dset_test_data_path + 'image_*.png')
     dset_test_names.sort()
     dset_test_label_path = '/home/dennis/indoor_localization/data/7Scenes/' + scene + '/01.txt'
     data = np.loadtxt(dset_test_label_path, usecols=range(1, 8))
-    print(data.shape)
     dset_test_num = data.shape[0]
     position = data[:, 0:3]
     orientation = np.zeros((dset_test_num, 3, 3))
     for i in range(dset_test_num):
         r = R.from_quat(data[i, 3:])
         orientation[i, :, :] = r.as_matrix().T
+    test = {}
+    test['names'] = dset_test_names
+    test['position'] = position
+    test['orientation'] = orientation
+    print('=>>>>>>>>>>>>>>>>>>>>>>Loading training data')
+    dset_train_data_path = '/home/dennis/indoor_localization/data/7Scenes/' + scene + '/sequences/00/'
+    dset_train_names = glob.glob(dset_train_data_path + 'image_*.png')
+    dset_train_names.sort()
+    dset_train_label_path = '/home/dennis/indoor_localization/data/7Scenes/' + scene + '/00.txt'
+    data = np.loadtxt(dset_train_label_path, usecols=range(1, 8))
+    dset_train_num = data.shape[0]
+    position = data[:, 0:3]
+    orientation = np.zeros((dset_train_num, 3, 3))
+    for i in range(dset_train_num):
+        r = R.from_quat(data[i, 3:])
+        orientation[i, :, :] = r.as_matrix().T
+    train = {}
+    train['names'] = dset_train_names
+    train['position'] = position
+    train['orientation'] = orientation
 
-    return None
-    # filepath = [driverpath subdataset '/sequences/01/image_'];
-    # posepath = [driverpath subdataset '/01.txt'];
-    # filepath_history = [driverpath subdataset '/sequences/00/image_'];
-    # posepath_history = [driverpath subdataset '/00.txt'];
-    #
-    # FILENAME_BASE = [driverpath subdataset '/posenet_training_output/cdf/chess_train__siamese_FXPAL_output'];
-    #
-    # for i = 1: 2
-    # filename = [FILENAME_BASE '_' int2str(i - 1) '.h5py'];
-    # pose_ID_groudtruth
-    # {i} = h5read(filename, '/posenet_x_label');
-    # pose_ID_predict
-    # {i} = h5read(filename, '/posenet_x_predicted');
-    # return X_train, y_train, X_test, y_test
+    dset = {}
+    dset['train'] = train
+    dset['test'] = test
 
-
-
-pass
+    print('=>>>>>>>>>>>>>>>>>>>>>>Loading 7Scenes descriptor result')
+    filename = "/home/dennis/indoor_localization/data/7Scenes/chess/posenet_training_output/cdf/chess_train__siamese_FXPAL_output_1.h5py"
+    with h5py.File(filename, "r") as f:
+        file_list = list(f.keys())
+        posenet_x_predicted = np.array(f[file_list[1]])
+    print('=>>>>>>>>>>>>>>>>>>>>>>Loading 7Scenes data finished')
+    return dset, posenet_x_predicted
 if __name__ == '__main__':
     subdataset = 'chess'
-    # load_7scenes_data()
     print('hello world from data loader!')
-    test_data_path = '/home/dennis/indoor_localization/data/7Scenes/' + subdataset + '/sequences/01/'
-    mylist = glob.glob(test_data_path + 'image_*.png')
-    mylist.sort()
+    sevenscenes, predicted = load_7scenes_data(subdataset)
 
-    load_7scenes_data(subdataset)
