78d1c992139e41c4597252bc3e9c074ecd5b39db
Man Yuan <manyuan@0587376921.wireless.umich.net>
2022-03-23 14:23:58 -0400

    tested all functions in the mathfunc.py, fixed some bugs during the testing

--- utils/mathfunc.py
+++ utils/mathfunc.py
@@ -73,7 +73,7 @@ def hat(x):
         x: 1d numpy array in R^6
     '''
 
-    l1 = np.hstack((skew(x[0:3]), x[3:6]))
+    l1 = np.hstack((skew(x[0:3]), x[3:6].reshape(-1, 1)))
     l2 = np.array([0, 0, 0, 0])
     return np.vstack((l1, l2))
 
@@ -85,7 +85,7 @@ def Adjoint_SE3(X):
         X: 4x4 matrix in SE(3)
     '''
 
-    l1 = np.hstack((X[0:3, 0:3], np.zeros(3, 3)))
+    l1 = np.hstack((X[0:3, 0:3], np.zeros((3, 3))))
     l2 = np.hstack((skew(X[0:3, 3]) @ X[0:3, 0:3], X[0:3, 0:3]))
     return np.vstack((l1, l2))
 
@@ -135,18 +135,6 @@ def LeftJacobian_SE3(xi):
     return np.vstack((l1, l2))
 
 
-def RightJacobianInverse_SE3(xi):
-    '''
-    Input:
-        xi: 1d numpy array with length of 6
-    '''
-
-    Jr = RightJacobian_SE3(xi)
-    ### TODO: check if this gives the same answer as in the matlab (Jr \ eye(size(Jr))
-    output = np.linalg.lstdq(Jr, np.eye(Jr.shape[0]))[0]
-    return output
-
-
 def RightJacobian_SE3(xi):
     '''
     Input:
@@ -157,10 +145,23 @@ def RightJacobian_SE3(xi):
     return output
 
 
-def calc_Jr(phi):
+def RightJacobianInverse_SE3(xi):
     '''
     Input:
-        phi: 1x3 matrix
+        xi: 1d numpy array with length of 6
+    '''
+
+    Jr = RightJacobian_SE3(xi)
+    ### TODO: check if this gives the same answer as in the matlab (Jr \ eye(size(Jr))
+    # output = np.linalg.lstsq(Jr, np.eye(Jr.shape[0]))[0]
+    output = np.linalg.inv(Jr)
+    return output
+
+
+def calc_Jl(phi):
+    '''
+    Input:
+        phi: 1x3 or 3x1 matrix
     '''
 
     if np.linalg.norm(phi) == 0:
@@ -176,7 +177,7 @@ def calc_Jr(phi):
 def calc_Jr(phi):
     '''
     Input:
-        phi: 1x3 matrix
+        phi: 1x3 or 3x1 matrix
     '''
 
     if np.linalg.norm(phi) == 0:
@@ -210,15 +211,15 @@ def angleDifference_so3(s):
     '''
     Computes the angle difference of the twist s
     Input:
-        s: 1x3 so(3) twist
+        s: 1x3 so(3) angle
     '''
 
     deltaR = expm(skew(s))
     epsilon = 1e-10
-    if(abs(trace(deltaR) - 3) < epsilon):
+    if(abs(np.trace(deltaR) - 3) < epsilon):
         angle = 0.
     else:
-        angle = np.ardcos((np.trace(deltaR) - 1) / 2) * 360 / 2 / np.pi
+        angle = np.arccos((np.trace(deltaR) - 1) / 2) * 360 / 2 / np.pi
     return angle
 
 
@@ -240,10 +241,51 @@ def disDifference(pt1, pt2):
 if __name__ == '__main__':
     # test cases
     # skew, getskew, unskew, getinvskew
-    x = np.array([1, 1, 1])
-    skew_x = skew(x)
-    print(skew_x)
-    print(getskew(x))
+    phi1 = np.array([1, 1, 1])
+    phi2 = np.array([2, 2, 2])
+    pt1  = np.array([1, 1, 1])
+    pt2  = np.array([2, 2, 2])
 
-    print(unskew(skew_x))
-    print(getinvskew(skew_x))
+    skew1 = skew(phi1)
+    skew2 = skew(phi2)
+
+    print(skew1)
+    print(getskew(phi1))
+
+    print(unskew(skew1))
+    print(getinvskew(skew1))
+
+    # wedge, hat
+    xi1 = np.hstack((phi1, pt1))
+    se3_1 = hat(xi1)
+    print(se3_1)
+    print(wedge(se3_1))
+
+    # Adjoint_SE3
+    print(Adjoint_SE3(expm(hat(xi1))))
+
+    # LeftJacobian_SO3
+    print(LeftJacobian_SO3(phi1))
+
+    # LeftJacobian_SE3
+    print(LeftJacobian_SE3(xi1))
+
+    # RightJacobian_SE3
+    Rse3_1 = RightJacobian_SE3(xi1)
+    print(Rse3_1)
+
+    # RightJacobianInverse_SE3
+    RIse3_1 = RightJacobianInverse_SE3(xi1)
+    print(RIse3_1)
+    print(RIse3_1@Rse3_1)
+
+    # calc_Jr, calc_Jl
+    print(calc_Jr(phi1))
+    print(calc_Jl(phi1))
+
+    # angleDifference, angleDifference_so3, disDifference
+    print(angleDifference(expm(skew1), expm(skew2)))
+
+    print(angleDifference_so3(phi1))
+
+    print(disDifference(pt1, pt2))
