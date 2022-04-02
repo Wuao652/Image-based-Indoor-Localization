550c9736b5038741d1cdf906343142b15a9338d2
dennis <wuaoliu52@gmail.com>
2022-04-01 20:27:32 -0400

    add opencv module

--- scripts/forward_process.py
+++ scripts/forward_process.py
@@ -2,17 +2,58 @@ import numpy as np
 import cv2
 from utils.dataloader_new import load_TUM_data
 from utils.keyframes_selection import selectimages
+import matplotlib.pyplot as plt
+
+from cyvlfeat.sift import sift as vlsift
+
+
+def vl_ubcmatch(template_descriptors, current_img_descriptors, match_thresh=1.5):
+    """
+
+    :param template_descriptors:
+    :param current_img_descriptors:
+    :param match_thresh:
+    :return:
+    """
+    flann_params = dict(algorithm=1, trees=4)
+    flann = cv2.flann_Index(current_img_descriptors, flann_params)
+    idx, dist = flann.knnSearch(template_descriptors, 2, params={})
+    del flann
+    matches = np.c_[np.arange(len(idx)), idx[:, 0]]
+    pass_filter = dist[:, 0]*match_thresh < dist[:, 1]
+    matches = matches[pass_filter]
+
+    return matches
+
+def plot_imgs(images):
+    """
+
+    :param images:
+    :return:
+    """
+    fig, ax = plt.subplots(2, 4)
+    for a in ax.reshape((-1)):
+      a.set_axis_off()
+    ax[0][0].imshow(cv2.imread(nImgList[0]))
+    ax[0][1].imshow(cv2.imread(nImgList[1]))
+    ax[0][2].imshow(cv2.imread(nImgList[2]))
+    ax[0][3].imshow(cv2.imread(nImgList[3]))
+    ax[1][0].imshow(cv2.imread(nImgList[4]))
+    ax[1][1].imshow(cv2.imread(nImgList[5]))
+    ax[1][2].imshow(cv2.imread(nImgList[6]))
+    ax[1][3].imshow(cv2.imread(nImgList[7]))
+    plt.show()
 def process_7scene_SIFT():
     pass
 if __name__ == "__main__":
     data_dict, posenet_x_predicted = load_TUM_data('1_desk2')
     gap = 2
-    num_image = 7
-    param = {}
-    param['bm'] = 0.1
-    param['sigma'] = 0.2
-    param['alpha_m'] = 3
-    param['max_range'] = 100
+    num_images = 7
+    params = {}
+    params['bm'] = 0.1
+    params['sigma'] = 0.2
+    params['alpha_m'] = 3
+    params['max_range'] = 100
 
     observe_ith_position = np.array([1.34420, 0.26860, 1.72490])
     observe_ith_orientation = np.array([[0.45171, 0.57196, -0.68471],
@@ -22,6 +63,37 @@ if __name__ == "__main__":
     observe_init_orientation = np.array([[0.45171, 0.57196, -0.68471],
                                         [0.89165, -0.26329, 0.36829],
                                         [0.03037, -0.77688, -0.62892]])
+    i = 0
     idx = 535
     # keyframes selection
+    idx_neighbor = selectimages(data_dict['train_position'], data_dict['train_orientation'], idx, params, num_images)
+    print(idx_neighbor)
+    num_img_left = (num_images - 1) / 2
+    nImgIndList = [i] + idx_neighbor
+    nImgList = [data_dict['train_images'][i] for i in nImgIndList]
+    print("Start to load img")
+    # plot_imgs(nImgList)
+    Iprev = cv2.imread(nImgList[0])
+    Iprev = cv2.cvtColor(Iprev, cv2.COLOR_BGR2GRAY)
+    Ipost = cv2.imread(nImgList[1])
+    Ipost = cv2.cvtColor(Ipost, cv2.COLOR_BGR2GRAY)
+# cyvlfeat
+    # kp1, des1 = vlsift(Iprev, edge_thresh=20, n_octaves=4, n_levels=6, compute_descriptor=True, float_descriptors=True)
+    # kp2, des2 = vlsift(Ipost, edge_thresh=20, n_octaves=4, n_levels=6, compute_descriptor=True, float_descriptors=True)
+# opencv
+    # sift = cv2.SIFT_create(nOctaveLayers=6, edgeThreshold=20)
+    # kp1, des1 = sift.detectAndCompute(Iprev, None)
+    # kp2, des2 = sift.detectAndCompute(Ipost, None)
+    # kp1_pt = [i.pt for i in kp1]
+    # kp2_pt = [i.pt for i in kp2]
+    # bf = cv2.BFMatcher()
+    # matches = bf.match(des1, des2)
+    # pts_1, pts_2 = [], []
+    # for i in range(len(matches)):
+    #     pts_1.append(kp1[matches[i].queryIdx].pt)
+    #     pts_2.append(kp2[matches[i].trainIdx].pt)
+    # pts_1, pts_2 = np.array(pts_1), np.array(pts_2)
+    # match_plot = cv2.drawMatches(Iprev, kp1, Ipost, kp2, matches, None, flags=2)
+    # plt.imshow(match_plot), plt.show()
+
 
