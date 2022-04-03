91c39b9dbddc61a3c733d0ce4f674127eb42f4f2
Man Yuan <zmangosix@gmail.com>
2022-04-03 14:40:01 -0400

    Finished triangulateMultiView, need to add reprojectionErrors

--- scripts/forward_process.py
+++ scripts/forward_process.py
@@ -2,9 +2,11 @@ import numpy as np
 import cv2
 from utils.dataloader_new import load_TUM_data
 from utils.keyframes_selection import selectimages
+from utils.triangulateMultiView import triangulateMultiView
+from utils.cameraParams import generateIntrinsics
 import matplotlib.pyplot as plt
 
-from cyvlfeat.sift import sift as vlsift
+# from cyvlfeat.sift import sift as vlsift
 
 
 def vl_ubcmatch(template_descriptors, current_img_descriptors, match_thresh=1.5):
@@ -69,31 +71,72 @@ if __name__ == "__main__":
     idx_neighbor = selectimages(data_dict['train_position'], data_dict['train_orientation'], idx, params, num_images)
     print(idx_neighbor)
     num_img_left = (num_images - 1) / 2
-    nImgIndList = [i] + idx_neighbor
-    nImgList = [data_dict['train_images'][i] for i in nImgIndList]
+    # train images only
+    nImgIndList = idx_neighbor
+    # The first one is the test image
+    nImgList = [data_dict['test_images'][i]] + [data_dict['train_images'][i] for i in nImgIndList]
     print("Start to load img")
     # plot_imgs(nImgList)
     Iprev = cv2.imread(nImgList[0])
     Iprev = cv2.cvtColor(Iprev, cv2.COLOR_BGR2GRAY)
     Ipost = cv2.imread(nImgList[1])
     Ipost = cv2.cvtColor(Ipost, cv2.COLOR_BGR2GRAY)
+    Ipost1 = cv2.imread(nImgList[2])
+    Ipost1 = cv2.cvtColor(Ipost1, cv2.COLOR_BGR2GRAY)
 # cyvlfeat
     # kp1, des1 = vlsift(Iprev, edge_thresh=20, n_octaves=4, n_levels=6, compute_descriptor=True, float_descriptors=True)
     # kp2, des2 = vlsift(Ipost, edge_thresh=20, n_octaves=4, n_levels=6, compute_descriptor=True, float_descriptors=True)
 # opencv
-    # sift = cv2.SIFT_create(nOctaveLayers=6, edgeThreshold=20)
-    # kp1, des1 = sift.detectAndCompute(Iprev, None)
-    # kp2, des2 = sift.detectAndCompute(Ipost, None)
-    # kp1_pt = [i.pt for i in kp1]
-    # kp2_pt = [i.pt for i in kp2]
-    # bf = cv2.BFMatcher()
-    # matches = bf.match(des1, des2)
-    # pts_1, pts_2 = [], []
-    # for i in range(len(matches)):
-    #     pts_1.append(kp1[matches[i].queryIdx].pt)
-    #     pts_2.append(kp2[matches[i].trainIdx].pt)
+    sift = cv2.SIFT_create(nOctaveLayers=6, edgeThreshold=20)
+    kp1, des1 = sift.detectAndCompute(Iprev, None)
+    kp2, des2 = sift.detectAndCompute(Ipost, None)
+    kp3, des3 = sift.detectAndCompute(Ipost1, None)
+
+    print(len(kp1))
+    print(len(kp2))
+    print(len(kp3))
+
+
+    bf = cv2.BFMatcher()
+    # Use these two matches to get a track
+    matches = bf.match(des1, des2)
+    matches1 = bf.match(des1, des3)
+
+    # Loop all the key points in test image
+    tracks = dict()
+
+    # for j in range(10):
+    #     print(matches[j].queryIdx)
+    #     print(matches1[j].queryIdx)
+
+    # for i in range(len(kp1)):
+    for i in range(3): # test 3 points only
+        tracks[i] = list()
+
+        id = matches[i].trainIdx
+        pt = kp2[id].pt
+        tracks[i].append((nImgIndList[0], pt))
+
+        id = matches1[i].trainIdx
+        pt = kp3[id].pt
+        tracks[i].append((nImgIndList[1], pt))
+    print(tracks)
+
+    camParams = generateIntrinsics()
+    camPoses = list()
+    camPoses.append({'ViewId': nImgIndList[0],
+                     'Orientation': data_dict['train_orientation'][nImgIndList[0]],
+                     'Location': data_dict['train_position'][nImgIndList[0]]})
+
+    camPoses.append({'ViewId': nImgIndList[1],
+                     'Orientation': data_dict['train_orientation'][nImgIndList[1]],
+                     'Location': data_dict['train_position'][nImgIndList[1]]})
+
+    print(camPoses)
+
+    xyz = triangulateMultiView(tracks, camPoses, camParams)
+    print(xyz)
+
     # pts_1, pts_2 = np.array(pts_1), np.array(pts_2)
     # match_plot = cv2.drawMatches(Iprev, kp1, Ipost, kp2, matches, None, flags=2)
     # plt.imshow(match_plot), plt.show()
-
-
