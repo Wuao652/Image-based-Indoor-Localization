c8420602ade31ed40f6b08abb2a67b4a2e3d6d32
dennis <wuaoliu52@gmail.com>
2022-04-05 17:57:12 -0400

    first commit for optimization_GL

---
+++ scripts/optimization_GL.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# TODO:
+# input : K camera intrinsic
+#         num_feature
+#         orientation
+#         position
+#         pts3d
+#         pts2d
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
